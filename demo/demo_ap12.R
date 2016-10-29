947b778a15314f3b1599efeed51896504de79755
Rainer Machne <raim@tbi.univie.ac.at>
2016-10-29 15:41:16 +0200

    seeting bar before boxplots

--- demo/demo_ap12.R
+++ demo/demo_ap12.R
@@ -35,6 +35,7 @@ names(lag) <- groups$EVC
 data <- shiftData(data, lag=lag)
 viewGroups(data,groups2=groups,lwd.orig=0,nrow=1)
 
+par(mai=c(1,1,.1,.1))
 boxData(data,rng=7,groups=groups,did="YFP")
 boxData(data,rng=7,groups=groups,did="YFP",type="bar")
 
@@ -46,5 +47,6 @@ viewGroups(data,groups2=groups,lwd.orig=0,dids=c("OD","YFP/OD"))
 od.data <- interpolatePlateData(data, xid="OD")
 viewGroups(od.data,groups2=groups,dids=c("YFP","YFP/OD"))
 
+par(mai=c(1,1,.1,.1))
 boxData(od.data,rng=0.7,groups=groups,did="YFP/OD",type="bar")
 
